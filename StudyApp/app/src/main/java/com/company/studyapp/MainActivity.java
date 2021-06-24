b0d9d3d7ca33071c443c49a2c78ece2c3b6789e4
Toosan <734429343@qq.com>
2021-06-25 00:15:31 +0800

    添加图片转Base64码

--- StudyApp/app/src/main/java/com/company/studyapp/MainActivity.java
+++ StudyApp/app/src/main/java/com/company/studyapp/MainActivity.java
@@ -18,20 +18,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        initStepView();
-       initMoveRect();
+        initStepView();
     }
 
-    private void initMoveRect() {
-        MoveRectView rectView = findViewById(R.id.move_rect);
-        rectView.setRect(100,200,300,400);
-        new Handler(getMainLooper()).postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                rectView.setRect(200,300,400,500);
-            }
-        },2000);
-    }
+
 
     private void initStepView() {
         StepView stepView = findViewById(R.id.step_view);
@@ -39,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
         // 设置动画 ofFloat 动画变动区间
         ValueAnimator animator = ValueAnimator.ofFloat(0,5000);
         // 动画从开始到结束的时间
-        animator.setDuration(5000);
+        animator.setDuration(1500);
         // 不断获取变化区间改变时的值
         animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
             @Override
--- StudyApp/app/src/main/java/com/company/studyapp/utils/BaseUtils.java
+++ StudyApp/app/src/main/java/com/company/studyapp/utils/BaseUtils.java
@@ -3,11 +3,87 @@ package com.company.studyapp.utils;
 import android.content.Context;
 import android.util.TypedValue;
 
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Objects;
+
 public class BaseUtils {
+
     public static int sp2px(Context context, int sp) {
         return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP,sp,context.getResources().getDisplayMetrics());
     }
     public static int dp2px(Context context, int dp) {
         return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,dp,context.getResources().getDisplayMetrics());
+    }
+
+    /**
+     * 网络图片转换Base64的方法
+     *
+     * @param netImagePath     
+     */
+    private static void NetImageToBase64(String netImagePath) {
+        final ByteArrayOutputStream data = new ByteArrayOutputStream();
+        try {
+            // 创建URL
+            URL url = new URL(netImagePath);
+            final byte[] by = new byte[1024];
+            // 创建链接
+            final HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setConnectTimeout(5000);
+
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        InputStream is = conn.getInputStream();
+                        // 将内容读取内存中
+                        int len = -1;
+                        while ((len = is.read(by)) != -1) {
+                            data.write(by, 0, len);
+                        }
+                        // 对字节数组Base64编码
+                        Base64Encoder encoder = new Base64Encoder();
+                        String strNetImageToBase64 = encoder.encode(data.toByteArray());
+                        System.out.println("网络图片转换Base64:" + strNetImageToBase64);
+                        // 关闭流
+                        is.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    /**
+     * 本地图片转换Base64的方法
+     *
+     * @param imgPath     
+     */
+
+    private static void ImageToBase64(String imgPath) {
+        byte[] data = null;
+        // 读取图片字节数组
+        try {
+            InputStream in = new FileInputStream(imgPath);
+            data = new byte[in.available()];
+            in.read(data);
+            in.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // 对字节数组Base64编码
+        Base64Encoder encoder = new Base64Encoder();
+        // 返回Base64编码过的字节数组字符串
+        System.out.println("本地图片转换Base64:" + encoder.encode(Objects.requireNonNull(data)));
     }
 }
